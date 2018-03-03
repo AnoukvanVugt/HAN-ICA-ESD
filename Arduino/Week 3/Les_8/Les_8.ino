e8984cdb0cd3d499fe1bc26c1a44518f8db121f0
AnoukvanVugt <anoukvvugt@hotmail.com>
2018-03-03 19:28:19 +0100

    Update les 8

--- Arduino/Week 3/Les_8/Les_8.ino
+++ Arduino/Week 3/Les_8/Les_8.ino
@@ -1,27 +1,68 @@
+unsigned long previousMillis1; //previousMillis for the tracking of the passing of one second for generating the illumination
+unsigned long previousMillis10; //previousMillis for the tracking of the passing of ten seconds for generating the illumination
+unsigned long previousMillis5; //previousMillis for the tracking of the passing of five seconds for generating the values for sending
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  randomSeed(analogRead(0));
+  previousMillis1 = millis();
+  previousMillis10 = millis();
+  previousMillis5 = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  verstuurWaardesMetChecksum("28;-15;6", 8);
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis5 >= 5000) { //5 seconden
+    previousMillis5 = currentMillis;
+    int temperature = generateRandomNumber(0, 30);
+    int illumination = generateIllumination();
+    char winddirection = generateWinddirection();
+    char values[] = {temperature, illumination, winddirection};
+    sendValuesUsingChecksum(values, 3);
+  }
 }
 
-char genereerChecksum(char waardes[], int nWaardes) {
+int generateRandomNumber(int minimum, int maximum) {
+  return random(minimum, maximum);
+}
+
+char generateWinddirection() {
+  char winddirections[] = {'N', 'E', 'S', 'W'};
+  return winddirections[random(0, 4)];
+}
+
+int generateIllumination() {
+  unsigned long currentMillis = millis();
+  int value = 0;
+  int returnValue = 0;
+  if (currentMillis - previousMillis1 >= 1000) { //1 seconde
+    previousMillis1 = currentMillis;
+    value += generateRandomNumber(0, 126);
+  }
+  if (currentMillis - previousMillis10 >= 10000) { //10 seconden
+    previousMillis10 = currentMillis;
+    returnValue = value / 10;
+    value = 0;
+  }
+  return returnValue;
+}
+
+char generateChecksum(char values[], int nValues) {
   char sum;
-  for (int i = 0; i < nWaardes; i++) {
-    sum = (sum + waardes[i]) % 128;
+  for (int i = 0; i < nValues; i++) {
+    sum = (sum + values[i]) % 127;
   }
   return sum;
 }
 
-void verstuurWaardesMetChecksum(char waardes[], int nWaardes) {
-  for (int i = 0; i < nWaardes; i++) {
-    Serial.write(waardes[i]);
+void sendValuesUsingChecksum(char values[], int nValues) {
+  Serial.write("^");
+  for (int i = 0; i < nValues; i++) {
+    Serial.write(values[i]);
   }
-  Serial.write(",");
-  Serial.write(genereerChecksum(waardes, nWaardes));
-  Serial.write('\n');
+  Serial.write(generateChecksum(values, nValues));
+  Serial.write(127);
 }
 
